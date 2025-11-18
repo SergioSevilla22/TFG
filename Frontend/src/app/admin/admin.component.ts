5d0ca022a795dc9775b555f2c241272d0ceaf8bd
MarcosRN03 <idlymarcboll@gmail.com>
2025-11-18 13:14:54 +0100

    Gestion admin

--- Frontend/src/app/admin/admin.component.ts
+++ Frontend/src/app/admin/admin.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
 import { HeaderComponent } from '../header/header.component';
@@ -7,7 +7,7 @@ import { HeaderComponent } from '../header/header.component';
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [CommonModule,FormsModule, HeaderComponent],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, HeaderComponent],
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
@@ -27,6 +27,16 @@ export class AdminComponent {
   // Modificar rol
   nuevoRol: string = "";
   resultadoRol: string = "";
+
+  RegisterForm = new FormGroup({
+    DNI: new FormControl('', Validators.required),
+    nombre: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    telefono: new FormControl('', [Validators.required]),
+    Rol: new FormControl('usuario')
+  });
+
+  selectedFile: File | null = null;
 
   constructor(private authService: AuthService) {}
 
@@ -97,6 +107,63 @@ export class AdminComponent {
       },
       error: (err) => {
         this.resultadoRol = err.error?.message || "Error al actualizar rol";
+      }
+    });
+  }
+
+    get DNI() { return this.RegisterForm.get('DNI'); }
+  get email() { return this.RegisterForm.get('email'); }
+
+  onSubmit() {
+    if (this.RegisterForm.invalid) {
+      this.RegisterForm.markAllAsTouched();
+      return;
+    }
+
+    const userData = this.RegisterForm.value as {
+      DNI: string;
+      nombre: string;
+      email: string;
+      telefono: string;
+      Rol?: string;
+    };
+
+    this.authService.register(userData).subscribe({
+      next: (res) => {
+        alert('Usuario registrado correctamente');
+        console.log('Registro:', res);
+      },
+      error: (err) => {
+        console.error('Error en registro:', err);
+        alert(err.error.message || 'Error al registrar usuario');
+      }
+    });
+  }
+
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.selectedFile = input.files[0];
+    }
+  }
+
+  uploadCSV() {
+    if (!this.selectedFile) {
+      alert("Por favor selecciona un archivo CSV.");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', this.selectedFile);
+
+    this.authService.registerMassive(formData).subscribe({
+      next: (res: any) => {
+        alert(res.message);
+        console.log(res);
+      },
+      error: (err) => {
+        console.error('Error en registro masivo:', err);
+        alert(err.error.message || 'Error al registrar usuarios');
       }
     });
   }
